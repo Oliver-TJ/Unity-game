ed8112ca9348ec3aa8d45268b19c51f1d3658e99
Harrison <harrisonrigby@icloud.com>
2021-01-20 20:46:31 +0000

    working chest detection
    need to add loot table and button for chest, cap point will work once "Only assignment, call, increment, decrement, await, and new object expressions can be used as a statement" is fixed

---
+++ Assets/CapturePoint.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CapturePoint : Interactable
+{
+    public bool isCaptured = false;
+    public float timeToCap = 10;
+
+    void Update(float timeToCap)
+    {
+        if (playerInRange && timeToCap > 0)
+        {
+            timeToCap -= Time.deltaTime;
+        }
+        if (!playerInRange)
+            timeToCap = 10;
+
+    }
+
+    public void Capping(float timeToCap)
+    {
+        if(timeToCap > 0)
+        {
+            isCaptured == true;
+            Debug.Log("Captured");
+        }
+    }
+
+
+
+}
