96b30630e10f8b3a9bf99fbc40bee86fec4cc36e
OliverTaiteJupp <07otaite-jupp@brightoncollege.net>
2021-01-28 21:38:43 +0000

    Added captureBar
    made the capture bar work

--- Assets/Scripts/CapturePoint.cs
+++ Assets/Scripts/CapturePoint.cs
@@ -1,21 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CapturePoint : Interactable
 {
     public bool isCaptured = false;
     public float timeToCap = 10;
+    [SerializeField] public Slider slider;
+    private float _duration;
 
     void Update()
     {
+        slider.value += _duration;
         if (playerInRange && timeToCap > 0)
         {
             timeToCap -= Time.deltaTime;
+            _duration = Time.deltaTime/10;
         }
-        if (!playerInRange)
-            timeToCap = 10;
 
+        if (!playerInRange)
+        {
+            timeToCap = 10;
+            _duration = 0;
+        }
     }
 
     public void Capping(float timeToCap)
