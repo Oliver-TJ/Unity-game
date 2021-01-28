e2b662a01e5bcecf7c91ffde863b9457d8b203c3
OliverTaiteJupp <07otaite-jupp@brightoncollege.net>
2021-01-28 13:46:06 +0000

    Added a minimap

--- Assets/Scripts/RTSController.cs
+++ Assets/Scripts/RTSController.cs
@@ -1,7 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using System; 
+using System;
+using Unity.Mathematics;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class RTSController : MonoBehaviour
 {
@@ -9,8 +11,11 @@ public class RTSController : MonoBehaviour
     [SerializeField] private GameObject pathPointer; 
     [SerializeField] private GameObject aimIndicator;
     [SerializeField] private GameObject fireball;
-    [SerializeField] private GameObject arrow; 
-    [SerializeField] private Camera view; 
+    [SerializeField] private GameObject arrow;
+    [SerializeField] private GameObject archers;
+    [SerializeField] private GameObject highlightSquare;
+    [SerializeField] private Camera view;
+    [SerializeField] private Grid grid;
     [SerializeField] private KeyCode[] keyset; 
     
     private Ability[] abilitySet; 
@@ -23,9 +28,9 @@ public class RTSController : MonoBehaviour
     {
         kingMethods = King.GetComponent<Actions>();
         kingRB = King.GetComponent<Rigidbody2D>();
-        abilitySet = new Ability[] { Fireball };
-        abilityPos = new Vector3[] { new Vector3(0, 0, 0) };
-        abilityUI = new List<GameObject>[] { new List<GameObject>() };
+        abilitySet = new Ability[] { Fireball, Archers };
+        abilityPos = new Vector3[] { Vector3.zero, Vector3.zero };
+        abilityUI = new List<GameObject>[] { new List<GameObject>(), new List<GameObject>() };
     }
 
     // Update is called once per frame
@@ -38,8 +43,7 @@ public class RTSController : MonoBehaviour
         if (Input.GetMouseButtonDown(1)) {
             Vector2 intent = (Vector2)view.ScreenToWorldPoint(Input.mousePosition); 
             Instantiate(pathPointer, new Vector3(intent.x, intent.y, 0), Quaternion.identity);
-            kingMethods.setIntent(intent); 
-            Debug.Log($"Mouse button down at {Input.mousePosition}");
+            kingMethods.setIntent(intent);
         }
 
         if (Input.GetKey(KeyCode.Space)) {
@@ -51,7 +55,6 @@ public class RTSController : MonoBehaviour
 
     void Fireball(int keyInd) {
         if (Input.GetKeyDown(keyset[keyInd])) {
-            // TODO: Instantiate pointer UI, updating depending on mouse position
             Vector2 pos = view.ScreenToWorldPoint(Input.mousePosition);
             abilityPos[keyInd] = new Vector3(pos.x, pos.y, 0);
             GameObject tc = Instantiate(aimIndicator, abilityPos[keyInd], Quaternion.identity);
@@ -62,7 +65,7 @@ public class RTSController : MonoBehaviour
             Vector2 dp = view.ScreenToWorldPoint(Input.mousePosition) - abilityPos[keyInd];
             abilityUI[keyInd][1].transform.rotation = Quaternion.Euler(0, 0, 360f*(float)Math.Atan2(-(double)dp.x, (double)dp.y) / (2*(float)Math.PI));
             if (dp.magnitude > 1.75) {
-                abilityUI[keyInd][1].transform.localScale = new Vector3(0.2f + 0.5f * (1 - (float)Math.Exp(-(dp.magnitude-1.8)/10)), 0.582f, 1);
+                abilityUI[keyInd][1].transform.localScale = new Vector3(0.2f + 0.65f * (1 - (float)Math.Exp(-(dp.magnitude-1.8)/10)), 0.582f, 1);
             } else {
                 abilityUI[keyInd][1].transform.localScale = new Vector3(0.2f, dp.magnitude / 1.8f * 0.582f, 1);
             }
@@ -74,6 +77,38 @@ public class RTSController : MonoBehaviour
             foreach (GameObject g in abilityUI[keyInd]) {
                 Destroy(g);
             }
+        }
+    }
+
+    void Archers(int keyInd)
+    {
+        if (Input.GetKeyDown(keyset[keyInd])) {
+            abilityPos[keyInd] = view.ScreenToWorldPoint(Input.mousePosition);
+            abilityPos[keyInd] = new Vector3((float)Math.Floor(abilityPos[keyInd].x)+0.5f, (float)Math.Floor(abilityPos[keyInd].y)+0.5f, 0);
+            GameObject a = Instantiate(archers, abilityPos[keyInd], Quaternion.identity);
+            GameObject b = Instantiate(highlightSquare, abilityPos[keyInd], Quaternion.identity);
+            abilityUI[keyInd] = new List<GameObject>() { a, b };
+
+        } else if (Input.GetKey(keyset[keyInd])) {
+            Vector3 pos = view.ScreenToWorldPoint(Input.mousePosition); // Get current mouse position 
+            pos = new Vector3((float)Math.Floor(pos.x)+0.5f, (float)Math.Floor(pos.y)+0.5f, 0); 
+            if (pos != abilityPos[keyInd]) // Check that the position has changed and it is now only a tile which isn't an obstacle
+            {
+                Destroy(abilityUI[keyInd][1]);
+                abilityUI[keyInd][1] = Instantiate(highlightSquare, pos, Quaternion.identity); // Set current tile to be highlighted
+                abilityUI[keyInd][0].transform.position = pos;
+                abilityPos[keyInd] = pos; 
+                Debug.Log($"Created tile at {pos}");
+            }
+        }
+
+        if (Input.GetKeyUp(keyset[keyInd]))
+        {
+            Vector3 pos = view.ScreenToWorldPoint(Input.mousePosition); // Get current mouse position 
+            pos = new Vector3((float)Math.Floor(pos.x)+0.5f, (float)Math.Floor(pos.y)+0.5f, 0); 
+            Destroy(abilityUI[keyInd][1]);
+            ArcherSpawning s = abilityUI[keyInd][0].GetComponent<ArcherSpawning>();
+            s.initialise();
         }
     }
 }
