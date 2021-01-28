6ecf8239049ad6d77a459fca51e0b1c82cce2234
JamesCXH <70763201+JamesCXH@users.noreply.github.com>
2021-01-28 12:59:02 +0000

    range

---
+++ Assets/EnemyHits.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHits : MonoBehaviour
+{
+    [SerializeField] float speed;
+    [SerializeField] float findRange;
+    [SerializeField] Transform player;
+
+    [SerializeField] float stoppingDistance;
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector2.Distance(transform.position, player.position) < findRange)
+        {
+            if (Vector2.Distance(transform.position, player.position) > stoppingDistance)
+            {
+                transform.position = Vector2.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
+            }
+        }
+    }
+}
--- Assets/Scripts/EnemyMoves.cs
+++ Assets/Scripts/EnemyMoves.cs
@@ -7,6 +7,7 @@ public class EnemyMoves : MonoBehaviour
     [SerializeField] float speed;
     [SerializeField] float stoppingDistance;
     [SerializeField] float retreatDistance;
+    [SerializeField] float findRange;
 
     private float shotInterval;
     [SerializeField] float shotStartInterval;
@@ -24,28 +25,28 @@ public class EnemyMoves : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Vector2.Distance(transform.position, player.position) > stoppingDistance)
-        {
-            transform.position = Vector2.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
-        } else if (Vector2.Distance(transform.position, player.position) < stoppingDistance &&
-                   Vector2.Distance(transform.position, player.position) > retreatDistance)
-        {
-            transform.position = this.transform.position;
-        } else if (Vector2.Distance(transform.position, player.position) < retreatDistance)
-        {
-            transform.position = Vector2.MoveTowards(transform.position, player.position, -speed * Time.deltaTime);
-        }
-
-        if (shotInterval <= 0)
-        {
-            Instantiate(projectile, transform.position, Quaternion.identity);
-            shotInterval = shotStartInterval;
-        }
-        else
-        {
-            shotInterval -= Time.deltaTime;
-        }
-
+        if (Vector2.Distance(transform.position, player.position) < findRange){
+			if (Vector2.Distance(transform.position, player.position) > stoppingDistance)
+        	{
+            	transform.position = Vector2.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
+        	} else if (Vector2.Distance(transform.position, player.position) < stoppingDistance &&
+                   		Vector2.Distance(transform.position, player.position) > retreatDistance)
+        	{
+            	transform.position = this.transform.position;
+        	} else if (Vector2.Distance(transform.position, player.position) < retreatDistance)
+        	{
+            	transform.position = Vector2.MoveTowards(transform.position, player.position, -speed * Time.deltaTime);
+        	}
+		
+        	if (shotInterval <= 0)
+        	{
+            	Instantiate(projectile, transform.position, Quaternion.identity);
+            	shotInterval = shotStartInterval;
+        	}
+        	else
+        	{
+            	shotInterval -= Time.deltaTime;
+        	}
+		}
     }
-
 }
