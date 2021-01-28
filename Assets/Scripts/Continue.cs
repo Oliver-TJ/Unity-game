c85af68275605f46018eb1aa7b78459eae0347c7
OliverTaiteJupp <07otaite-jupp@brightoncollege.net>
2021-01-28 21:24:26 +0000

    Menu
    Added death scenes and main menu

---
+++ Assets/Scripts/Continue.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Continue : MonoBehaviour
+{
+    public void OnButtonPress()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+}
