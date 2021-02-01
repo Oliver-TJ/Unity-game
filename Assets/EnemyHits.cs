bd618013b01d86cec923171111c2f862647987c6
JamesCXH <70763201+JamesCXH@users.noreply.github.com>
2021-02-01 09:00:45 +0000

    EnemyHitsstillBroken

--- Assets/EnemyHits.cs
+++ Assets/EnemyHits.cs
@@ -7,22 +7,60 @@ public class EnemyHits : MonoBehaviour
     [SerializeField] float speed;
     [SerializeField] float findRange;
     [SerializeField] Transform player;
-
     [SerializeField] float stoppingDistance;
+    [SerializeField] float dashSpeed;
+    private Vector2 tempPos;
+    private Vector2 targetPos;
+    private Rigidbody2D rb;
+    private bool detected;
+    private bool primed;
+    
+    
+
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player").transform;
+        Vector2 tempPos = new Vector2(transform.position.x, transform.position.y);
+        detected = false;
+        primed = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Vector2.Distance(transform.position, player.position) < findRange)
+        Vector2 tempPos = new Vector2(transform.position.x, transform.position.y);
+
+        if (detected == false && Vector2.Distance(transform.position, player.position) < findRange)
+        {
+            detected = true;
+        }
+
         {
             if (Vector2.Distance(transform.position, player.position) > stoppingDistance)
             {
-                transform.position = Vector2.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
+                targetPos = new Vector2(player.position - transform.position);
+                rb.MovePosition(transform.position + targetPos * speed * Time.deltaTime);
+                
+                if (primed = false)
+                {
+                    primed = true;
+                }
             }
+            /*
+            else
+            {
+                if (primed == true && Vector2.Distance(transform.position, player.position) <= stoppingDistance)
+                {
+                    transform.position = Vector2.MoveTowards(transform.position, player.position, dashSpeed * Time.deltaTime);
+                }
+            }
+            */
         }
+    }
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        primed = false;
+        rb.velocity = new Vector2(0.0f, 0.0f);
+        rb.angularVelocity = (0.0f);
     }
 }
